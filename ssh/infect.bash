d08c6c01ec6c3e52356be48f0fd8abe92ad9bc7e
Bob Somers <bob@bobsomers.com>
2011-12-09 19:16:32 -0800

    Woops.

--- ssh/infect.bash
+++ ssh/infect.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Fixing permissions."
-chmod 600 ~/keys/authorized_keys
-chmod 600 ~/keys/config
+chmod 600 ~/dotfiles/ssh/authorized_keys
+chmod 600 ~/dotfiles/ssh/config
 
 echo "Making symlinks."
-ln -f -s ~/dotfiles/authorized_keys ~/.ssh/authorized_keys
-ln -f -s ~/dotfiles/config ~/.ssh/config
+ln -f -s ~/dotfiles/ssh/authorized_keys ~/.ssh/authorized_keys
+ln -f -s ~/dotfiles/ssh/config ~/.ssh/config
 
 echo "Done."
