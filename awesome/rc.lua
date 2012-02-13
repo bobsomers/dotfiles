47c59f184028a83c5fefb2ecc7c186a0fff53347
Bob Somers <bob@bobsomers.com>
2012-02-13 08:30:33 -0800

    I probably only think this is funny because I'm tired.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ "one", "two", "three", "four" }, s, layouts[1])
+    tags[s] = awful.tag({ "foo", "bar", "baz" }, s, layouts[1])
 end
 -- }}}
 
