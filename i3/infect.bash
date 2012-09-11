4f8cbaf84bfb1c3c923132222d676e8d401a5d63
Bob Somers <bob@bobsomers.com>
2012-09-11 01:00:18 -0700

    i3 status bar.

--- i3/infect.bash
+++ i3/infect.bash
@@ -3,5 +3,7 @@
 echo "Making symlinks."
 mkdir -p ~/.i3
 ln -f -s ~/dotfiles/i3/config ~/.i3/config
+mkdir -p ~/.config/i3status
+ln -f -s ~/dotfiles/i3/status ~/.config/i3status/config
 
 echo "Done."
