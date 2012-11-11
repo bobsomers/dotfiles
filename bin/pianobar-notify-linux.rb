5bdb6b769f7f27fbccab3f70b71d2bcc4353f83f
Bob Somers <bob@bobsomers.com>
2012-11-10 23:45:00 -0800

    Swap order of song/artist on pianobar notifications.

--- bin/pianobar-notify-linux.rb
+++ bin/pianobar-notify-linux.rb
@@ -6,5 +6,5 @@ if trigger == 'songstart'
    songinfo = {}
 
    STDIN.each_line {|line| songinfo.store(*line.chomp.split('=', 2))}
-   `notify-send -i /usr/share/icons/Faenza/emblems/16/emblem-sound.png "#{songinfo['artist']}" "#{songinfo['title']}"`
+   `notify-send -i /usr/share/icons/Faenza/emblems/16/emblem-sound.png "#{songinfo['title']}" "#{songinfo['artist']}"`
 end
