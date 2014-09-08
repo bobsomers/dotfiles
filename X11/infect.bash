700d5dcce3cab98c954e5a16b5e9ac039b561447
Bob Somers <bob@bobsomers.com>
2014-09-08 16:33:59 -0700

    Fix cursor size on higher DPI displays.

--- X11/infect.bash
+++ X11/infect.bash
@@ -2,6 +2,7 @@
 
 echo "Making symlinks."
 ln -f -s ~/dotfiles/X11/xinitrc ~/.xinitrc
-ln -f -s ~/dotfiles/X11/Xmodmap ~/.Xmodmap
+#ln -f -s ~/dotfiles/X11/Xmodmap ~/.Xmodmap
+ln -f -s ~/dotfiles/X11/Xresources ~/.Xresources
 
 echo "Done."
