f8d3a15eae730e1aae559e3cf4c574b061578674
Bob Somers <bob@bobsomers.com>
2014-09-27 23:44:29 -0700

    Split Krita keys from main Cintiq config. Also recalibrated.

---
+++ bin/cintiq-krita.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xsetwacom set "Wacom Cintiq 13HD pad" Button 2 "key +shift"
+xsetwacom set "Wacom Cintiq 13HD pad" Button 3 "key 5"
+
+xsetwacom set "Wacom Cintiq 13HD pad" Button 11 "key esc"
+xsetwacom set "Wacom Cintiq 13HD pad" Button 12 "key +ctrl z -ctrl"
+xsetwacom set "Wacom Cintiq 13HD pad" Button 1 "key tab"
+xsetwacom set "Wacom Cintiq 13HD pad" Button 10 "key +ctrl +shift z -shift -ctrl"
+# TODO: button 13?
+
+xsetwacom set "Wacom Cintiq 13HD pad" Button 8 "key 2"
+xsetwacom set "Wacom Cintiq 13HD pad" Button 9 "key +ctrl"
--- bin/cintiq.sh
+++ bin/cintiq.sh
@@ -5,18 +5,5 @@ xsetwacom set "Wacom Cintiq 13HD stylus" MapToOutput "1920x1080+0+1080"
 xsetwacom set "Wacom Cintiq 13HD eraser" MapToOutput "1920x1080+0+1080"
 
 # Account for screen calibration.
-xsetwacom set "Wacom Cintiq 13HD stylus" Area 551 412 58831 33125
-xsetwacom set "Wacom Cintiq 13HD eraser" Area 551 412 58831 33125
-
-# Map Wacom buttons to useful Krita keys.
-xsetwacom set "Wacom Cintiq 13HD pad" Button 2 "key +shift"
-xsetwacom set "Wacom Cintiq 13HD pad" Button 3 "key 5"
-
-xsetwacom set "Wacom Cintiq 13HD pad" Button 11 "key esc"
-xsetwacom set "Wacom Cintiq 13HD pad" Button 12 "key +ctrl z -ctrl"
-xsetwacom set "Wacom Cintiq 13HD pad" Button 1 "key tab"
-xsetwacom set "Wacom Cintiq 13HD pad" Button 10 "key +ctrl +shift z -shift -ctrl"
-# TODO: button 13?
-
-xsetwacom set "Wacom Cintiq 13HD pad" Button 8 "key 2"
-xsetwacom set "Wacom Cintiq 13HD pad" Button 9 "key +ctrl"
+xsetwacom set "Wacom Cintiq 13HD stylus" Area 374 300 59033 33250
+xsetwacom set "Wacom Cintiq 13HD eraser" Area 374 300 59033 33250 
