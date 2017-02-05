144dea3a4ced31c1a4e227d14c021bc08599eb0f
Bob Somers <bob@bobsomers.com>
2017-02-05 01:22:47 -0800

    Add pikusomers.com to log rotation.

--- bin/rotatelogs.bash
+++ bin/rotatelogs.bash
@@ -5,6 +5,8 @@ mv /srv/http/bobsomers.com/logs/access.log /srv/http/bobsomers.com/logs/$(date +
 mv /srv/http/bobsomers.com/logs/error.log /srv/http/bobsomers.com/logs/$(date +%F)_error.log
 mv /srv/http/flexrender.org/logs/access.log /srv/http/flexrender.org/logs/$(date +%F)_access.log
 mv /srv/http/flexrender.org/logs/error.log /srv/http/flexrender.org/logs/$(date +%F)_error.log
+mv /srv/http/pikusomers.com/logs/access.log /srv/http/pikusomers.com/logs/$(date +%F)_access.log
+mv /srv/http/pikusomers.com/logs/error.log /srv/http/pikusomers.com/logs/$(date +%F)_error.log
 
 # Tell nginx to reopen the logs.
 kill -USR1 $(cat /run/nginx.pid)
@@ -17,3 +19,5 @@ bzip2 /srv/http/bobsomers.com/logs/$(date +%F)_access.log
 bzip2 /srv/http/bobsomers.com/logs/$(date +%F)_error.log
 bzip2 /srv/http/flexrender.org/logs/$(date +%F)_access.log
 bzip2 /srv/http/flexrender.org/logs/$(date +%F)_error.log
+bzip2 /srv/http/pikusomers.com/logs/$(date +%F)_access.log
+bzip2 /srv/http/pikusomers.com/logs/$(date +%F)_error.log
