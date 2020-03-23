243b11ce0ab1c1ef94e8547d25b763e93a7d9f68
Bob Somers <bob@bobsomers.com>
2020-03-23 10:30:36 -0700

    dunst, greenclip, picom, tmux, X11

--- X11/infect.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Making symlinks."
-ln -f -s ~/dotfiles/X11/xinitrc ~/.xinitrc
-#ln -f -s ~/dotfiles/X11/Xmodmap ~/.Xmodmap
-ln -f -s ~/dotfiles/X11/Xresources ~/.Xresources
-
-echo "Done."
